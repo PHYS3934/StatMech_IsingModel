0ec423bb3170ef964bb079e1656f0b58d972b48b
Ben Fulcher <ben.d.fulcher@gmail.com>
2021-04-09 20:55:02 +1000

    Better plotting 

--- IsingPlot.m
+++ IsingPlot.m
@@ -3,6 +3,9 @@ function h_Image = IsingPlot(grid,N,J,kT,M,E,h_Image)
 %-------------------------------------------------------------------------------
 
 % Set default (set h_Image empty to initialize)
+if nargin < 6
+    N = [];
+end
 if nargin < 7
     h_Image = [];
 end
@@ -10,20 +13,23 @@ end
 %-------------------------------------------------------------------------------
 if isempty(h_Image)
     % Initialize:
-    h_Image = imagesc((grid+1)*128);
+    h_Image = imagesc(grid);
     title(sprintf('2D Ising model with %0.4g by %0.4g lattice',N,N));
     axis('square');
     colormap([0,0,0;1,1,1]);
     ax = gca();
+    ax.XTick = 1:length(grid);
+    ax.YTick = 1:length(grid);
     ax.XTickLabel = [];
     ax.YTickLabel = [];
     caxis([-1,1])
 else
     % Update
-    h_Image.CData = (grid+1)*128;
+    h_Image.CData = grid;
 end
 
-xlabel(sprintf('J = %0.2f, kT = %0.2f, M = %0.3f, E = %0.3f',J,kT,M,E));
+xlabel(sprintf('J = %0.2f, kT = %0.2f, M = %0.3f, E = %0.3f',...
+                J,kT,M,E));
 drawnow;
 
 end
