84c1f6bf2d27c8ce822928ac2bed50974ee49f76
Ben Fulcher <ben.d.fulcher@gmail.com>
2021-04-13 11:32:42 +1000

    Turn on/off normalizing correlation function

--- CorrelationFun.m
+++ CorrelationFun.m
@@ -1,9 +1,19 @@
-function cor = CorrelationFun(A)
+function cor = CorrelationFun(A,doNorm)
 % Computes the correlation function of A
+% INPUTS: - A: a square (spin) grid.
+%         - doNorm: (logical), whether to subtract the lattice-average M squared
 % Output is centered at size(A)/2 because of periodic boundary conditions
+%-------------------------------------------------------------------------------
+
+% Set default:
+if nargin < 2
+    doNorm = true;
+end
 
 cor = conv2(A,repmat(rot90(A,2),2,2),'same')/numel(A);
-cor = cor - (sum(sum(A))/numel(A))^2;
+if doNorm
+    cor = cor - mean(A(:))^2;
+end
 cor = circshift(cor,ceil(size(A)/2));
 
 end
